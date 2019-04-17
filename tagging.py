662d304ad0e36da0ac9f6a2886d3693f39a9c14d
Philip Eagles <pxe778@student.bham.ac.uk>
2019-04-17 18:05:30 +0100

    refactoring and tweaks

--- tagging.py
+++ tagging.py
@@ -38,8 +38,9 @@ def tag_paragraphs(text):
 
 def tag_sentences(text):
     output = text
-    # only tags within paragraphs
-    paragraphs = re.findall(r'(?:<paragraph>)((?:.|\n)*?)(?:<\/paragraph>)', text)
+    # only tags within paragraph tags
+    search_expression = r'(?:<paragraph>)((?:.|\n)*?)(?:<\/paragraph>)'
+    paragraphs = re.findall(search_expression, text)
     # iterate over paragraph text
     for p in paragraphs:
         # tokenize the sentences
@@ -52,37 +53,47 @@ def tag_sentences(text):
 
 def remove_single_new_lines(text):
     # negative lookahead and lookbehind for \n
-    output = re.sub(r'(?:^|[^\n])(\n)(?!\n)', " ", text)
+    search_expression = r'(?:^|[^\n])(\n)(?!\n)'
+    output = re.sub(search_expression, " ", text)
     return output
 
 
 def remove_email_text(text):
-    # removes lines surrounded by < and > or any word followed by a :
-    text = re.sub(r'(?:\n|^)((?:<.*>)|(?:[^\S\n]*\w+:.*))', "", text)
-    # removes lines with no text but some symbols
-    text = re.sub(r'(?:^|\n)([^\w\d\n]+)(?:$|\n)', "", text)
-    # removes lines with lots of whitespace before text
-    text = re.sub(r'(?:^)(?!\t\w)([^\S\n]{2,}[\w\d\t \r]+.*)(?:$)', "", text, flags=re.MULTILINE)
-    # removes lines with lots of whitespace in the middle
-    text = re.sub(r'(?:^|\n)(\w.+?[^\S\n]{6,}.+?\w)(?:$|\n)', "", text)
-    # removes lines starting with a time
-    text = re.sub(r'(?:^|\n)(\s+\d{1,2}(?:(?:[.:]{1}\d{2})|(?:\s?p\.?m\.?|\s?a\.?m\.?)){1,2}.*)(?:$|\n)', "", text)
-    # removes lines starting with a day (not case sensitive)
+    # remove lines surrounded by < and > or any word followed by a :
+    search_expression = r'(?:\n|^)((?:<.*>)|(?:[^\S\n]*\w+:.*))'
+    text = re.sub(search_expression, "", text)
+    # remove lines with no text but some symbols
+    search_expression = r'(?:^|\n)([^\w\d\n]+)(?:$|\n)'
+    text = re.sub(search_expression, "", text)
+    # remove lines with lots of whitespace before text
+    search_expression = r'(?:^)(?!\t\w)([^\S\n]{2,}[\w\d\t \r]+.*)(?:$)'
+    text = re.sub(search_expression, "", text, flags=re.MULTILINE)
+    # remove lines with lots of whitespace in the middle
+    search_expression = r'(?:^|\n)(\w.+?[^\S\n]{6,}.+?\w)(?:$|\n)'
+    text = re.sub(search_expression, "", text)
+    # remove lines starting with a time
+    search_expression = r'(?:^|\n)(\s+\d{1,2}(?:(?:[.:]{1}\d{2})|(?:\s?p\.?m\.?|\s?a\.?m\.?)){1,2}.*)(?:$|\n)'
+    text = re.sub(search_expression, "", text)
+    # remove lines starting with a day (not case sensitive)
     days_list = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
     # includes 3 letter variations
-    # e.g. sun (rather than Sunday
+    # e.g. mon (rather than Monday)
     days_string = '|'.join(day + "|" + day[:3] for day in days_list)
-    text = re.sub(r'(?:^|\n)(\s*(?:' + days_string + r').*)(?:$|\n)', "", text, flags=re.IGNORECASE)
-    # removes lines that are in all caps
-    text = re.sub(r'(?:^|\n)([\sA-Z\d:.,()[\]{}/\\!?\-"\'`]+)(?:$|\n)', "", text)
-    # removes lines that end in the word seminar
-    output = re.sub(r'(?:^|\n)(.*seminar)(?:$|\n)', "", text, flags=re.IGNORECASE)
-    # removes leading/trailing whitespace
+    search_expression = r'(?:^|\n)(\s*(?:' + days_string + r').*)(?:$|\n)'
+    text = re.sub(search_expression, "", text, flags=re.IGNORECASE)
+    # remove lines that are in all caps
+    # TODO: see if (?:^|\n)([^a-z]+)(?:$|\n) works
+    search_expression = r'(?:^|\n)([\sA-Z\d:.,()[\]{}/\\!?\-"\'`]+)(?:$|\n)'
+    text = re.sub(search_expression, "", text)
+    # remove lines that end in the word seminar
+    search_expression = r'(?:^|\n)(.*seminar\s*)(?:$|\n)'
+    output = re.sub(search_expression, "", text, flags=re.IGNORECASE)
+    # remove leading/trailing whitespace
     output = output.strip()
     return output
 
 
-# check that the input arguments exist
+# check if input arguments exist
 if len(sys.argv) != 3:
     print('usage: ' + sys.argv[0] + " input_files_directory output_files_directory")
     sys.exit(2)
@@ -92,10 +103,10 @@ output_files_path = sys.argv[2]
 if output_files_path[-1] != '/':
     output_files_path += '/'
 # check if the output directory exists
-# if not then creates it
+# if not then create it
 if not exists(output_files_path):
     makedirs(output_files_path)
-# gets the untagged files
+# get the untagged files
 untagged_text_files_list = [f for f in listdir(untagged_text_files_path) if isfile(join(untagged_text_files_path, f))]
 # iterate over all files
 for untagged_text_file_name in untagged_text_files_list:
